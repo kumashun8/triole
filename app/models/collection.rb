586817f73360036f260de3c5d67a8f74aea78ee1
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-30 11:49:41 +0900

    add validation to Collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,6 @@
 class Collection < ApplicationRecord
+  validates :title, {presence: true, length: {maximum: 30}}
+
+  has_many :recommends, dependent: :destroy
+  validates_associated :recommends
 end
