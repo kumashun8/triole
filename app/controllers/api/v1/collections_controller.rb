d0302ee251f48cbe883e79987572a2d15e1bc4d4
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-30 17:13:48 +0900

    create collections#create,index

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::CollectionsController < ApplicationController
   def create
+    @collection = Collection.create(collection_params)
+    render json: @collection
+  end
 
+  def index
+    @collections = Collection.all
+    render json: @collections
   end
 
   private
@@ -8,14 +14,14 @@ class Api::V1::CollectionsController < ApplicationController
     def collection_params
       params.require(:collection).permit(
         :title,
-        recommend: [
-          :name,
-          :price
-          shop: [
-            :name,
-            :googlemap_link
-          ]
-        ]
+        # recommend: [
+        #   :name,
+        #   :price
+        #   shop: [
+        #     :name,
+        #     :googlemap_link
+        #   ]
+        # ]
       )
     end
 end
