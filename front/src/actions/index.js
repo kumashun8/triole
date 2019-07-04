eb5809e1352f4f4ae848cee7b9090c4d8326baa9
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-04 18:45:45 +0900

    create action index

---
+++ front/src/actions/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+export const SearchStatuses = {
+  GET_COLLECTIONS_REQUEST: 'GET_COLLECTIONS_REQUEST',
+  GET_COLLECTIONS_SUCCESS: 'GET_COLLECTIONS_SUCCESS',
+  GET_COLLECTIONS_FAILUE: 'GET_COLLECTIONS_FAILUE'
+}
+
+export const getCollectionsRequest = () => ({
+  type: 'GET_COLLECTIONS_REQUEST'
+})
+
+export const getCollectionsSuccess = (json) => ({
+  type: 'GET_COLLECTIONS_SUCCESS',
+  collections: json,
+  receivedAt: Date.now()
+})
+
+export const getCollectionsFaiue = (error) => ({
+  type: 'GET_COLLECTIONS_FAILUE',
+  error
+})
+
+export const requestIsNotExist = () => ({
+  type: 'GET_COLLECTIONS_FAILUE'
+})
+
+export const getCollections = () => {
+  return (dispatch) => {
+    dispatch(getCollectionsRequest())
+    return axios.get(process.env.REACT_APP_API_URI + '/api/v1/collections/')
+      .then(res => {
+        console.log(res.data)
+        dispatch(getCollectionsSuccess(res.data))
+      }
+      ).catch(err => 
+      dispatch(getCollectionsFaiue(err))
+    )
+  }
+}
