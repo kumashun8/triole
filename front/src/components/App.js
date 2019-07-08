91ca3b0ead38d031d1ad1f4198866fdf6696c32b
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-08 10:33:12 +0900

    remove muda

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import ShopSearchInput from '../containers/ShopSearchInput'
-import PredictedShopList from '../containers/PredictedShopList'
-import ShopWrapper from '../components/ShopWrapper'
 import CollectionPostInput from '../containers/CollectionPostInput'
 import CollectionIndex from '../containers/CollectionIndex'
 
