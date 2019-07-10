45a577f0424e93d237122a72224bdaf79066338e
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-11 01:34:46 +0900

    set propTypes for Headline

--- front/src/components/Headline.js
+++ front/src/components/Headline.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Styles from '../styles/components/headline.module.scss';
+import PropTypes from 'prop-types';
 
 const Headline = ({ title }) => (
   <p className={Styles.headline}>{title}</p>
 )
+
+Headline.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 export default Headline
