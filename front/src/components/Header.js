c03f33de8596b513b72dd27709c0aee17e0680dd
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-11 00:48:48 +0900

    semi-kansei header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Styles from '../styles/components/header.module.scss'
+import logo from '../images/triole_logo.png'
 
 const Header = () => (
-  <header>
+  <header className={Styles.header}>
     <div className={Styles.header_menu}>
-      <img src="../images/triole_icon.png" alt="triole" />
+      <img className={Styles.header_menu_home} src={logo} alt="Triole" />
     </div>
   </header>
 )
