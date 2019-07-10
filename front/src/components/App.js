524a6c3a396d9899a37f16b4a84ed929776e5d18
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-11 00:53:37 +0900

    create style for app

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CollectionPostInput from '../containers/CollectionPostInput'
 import CollectionIndex from '../containers/CollectionIndex'
+import Styles from '../styles/components/app.module.scss'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={Styles.app}>
         <CollectionPostInput />
         <CollectionIndex />
       </div>
