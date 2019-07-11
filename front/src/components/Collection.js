006dc1950647869e2ebf6a26dab9cddd51a6264c
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-11 14:20:36 +0900

    create style collection

--- front/src/components/Collection.js
+++ front/src/components/Collection.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import Styles from '../styles/components/collection.module.scss'
 
 const Collection = ({ collection }) => (
-  <li>
-    <h2>{collection.title}</h2>
-    {collection.recommends.map( recommend => (
-      <div>
-        <p>{recommend.name}</p>
-        <p>{recommend.price}円</p>
-        <p>
-          <a
-            href={recommend.shop.googlemap_link}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {recommend.shop.name}
-          </a>
-        </p>
-      </div>
-    ))}
+  <li className={Styles.collectionWrapper}>
+    <div className={Styles.collection}>
+      <h2>{collection.title}</h2>
+      {collection.recommends.map( recommend => (
+        <div>
+          <p>{recommend.name}</p>
+          <p>{recommend.price}円</p>
+          <p><a
+              href={recommend.shop.googlemap_link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {recommend.shop.name}
+          </a></p>
+        </div>
+      ))}
+    </div>
   </li>
 )
 
