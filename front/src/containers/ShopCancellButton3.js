5286c893ea2ef4db19836624ffe469288086308b
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-11 12:37:18 +0900

    create container Shi

---
+++ front/src/containers/ShopCancellButton3.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux'
+import SmallButton from '../components/SmallButton'
+import { clearSelectedShop } from '../actions/detail'
+
+const mapStateToProps = state => ({
+  text: "x"
+})
+
+const mapDispatchToProps = dispatch => ({
+  dispatchCancellAction: value => dispatch(clearSelectedShop(3))
+})
+
+const ShopCancellButton3 = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SmallButton)
+
+export default ShopCancellButton3
