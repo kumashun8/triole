15ec93a9713e14be772faaa7ea743889d5a82e08
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-16 18:51:33 +0900

    update image_uploader

--- app/uploaders/reco_image_uploader.rb
+++ app/uploaders/reco_image_uploader.rb
@@ -6,6 +6,7 @@ class RecoImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  process convert: 'jpg'
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -35,13 +36,14 @@ class RecoImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
+  end
+
 end
