d78a480240347aab0f1754f890e0965268838f9a
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-29 00:41:36 +0900

    remove muda

--- front/src/components/Cover.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Styles from '../styles/components/cover.module.scss'
-
-const Cover = () => (
-  <div className={Styles.cover} height={document.getElementById('app').style.height}>
-  </div>
-)
-
-export default Cover
