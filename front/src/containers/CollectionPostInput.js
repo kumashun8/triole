64de5eb96b37887ffdc81919ef745fd91bf0f4aa
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-07-31 11:46:42 +0900

    update container collectionPostInput

--- front/src/containers/CollectionPostInput.js
+++ front/src/containers/CollectionPostInput.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import { postCollection } from '../actions/posting'
 import { clearShopList } from '../actions/search'
 import { clearSelectedShop } from '../actions/detail'
+import { updatePreview, clearPreview } from '../actions/preview'
 import PostingForm from '../components/PostingForm'
 
 const mapStateToProps = state => {
@@ -9,17 +10,22 @@ const mapStateToProps = state => {
   const currentState = state.post[length - 1]
   const length2 = state.selectedShop.length
   const currentState2 = state.selectedShop[length2 - 1]
+  const length3 = state.previews.length
+  const currentState3 = state.previews[length3 - 1]
   // console.log(currentState2)
   return {
     collection: currentState.collection,
-    shops: currentState2.shop
+    shops: currentState2.shop,
+    previews: currentState3.previews
   }
 }
 
 const mapDispatchToProps = dispatch => ({
   dispatchPostAction: collection => dispatch(postCollection(collection)),
   dispatchClearShopList: index => dispatch(clearShopList(index)),
-  dispatchClearSelectedShop: index => dispatch(clearSelectedShop(index))
+  dispatchClearSelectedShop: index => dispatch(clearSelectedShop(index)),
+  dispatchUpdatePreview: (index, imageUrl) => dispatch(updatePreview(index, imageUrl)),
+  dispatchClearPreview: index => dispatch(clearPreview(index))
 })
 
 const CollectionPostInput = connect(
